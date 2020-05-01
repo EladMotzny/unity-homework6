06ac7c1a4d822492ac34abcc30bec70e76673642
Anna <33619352+annty2@users.noreply.github.com>
2020-05-01 20:29:08 +0300

    Add 3 lives until destruction

---
+++ 02-collisions/Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class DestroyOnTrigger2D : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag;
+    [SerializeField] GameObject target;
+    [SerializeField] NumberField lives;
+    [SerializeField] int hits = 3;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+       
+        if (other.tag == triggeringTag && enabled) {
+
+            if (this.gameObject== target && triggeringTag== "Enemy" && hits >= 1 )
+            {
+                if (hits == 1)
+                { 
+                    Destroy(this.gameObject);   
+                }
+               hits--;
+               lives.SetNumber(hits);
+               Destroy(other.gameObject);
+
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                Destroy(other.gameObject);
+            }
+            
+        }
+    }
+}
---
+++ 02-collisions/Assets/Scripts/3-collisions/Lives.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour
+{
+
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object after 3 hits")]
+    [SerializeField] string triggeringTag;
+    [SerializeField] NumberField lives;
+    [SerializeField] int hits = 3;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == triggeringTag && enabled)
+        {
+               if (hits > 1)
+             {
+               hits--;
+             lives.SetNumber(hits);
+            Destroy(other.gameObject);
+
+              }
+
+              else
+              {
+              lives.SetNumber(hits--);
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+             }
+
+        }
+    }
+}
+
